829a574dfb4147910225da3f441f628d3c346fcf
Gorden-Dosi <gosangmin1@gmail.com>
2023-05-19 13:31:07 +0900

    fear: add .env

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 // import Calculater from './pages/Calculater';
-import TransForm from './pages/TransForm';
+import TransForm from "./pages/TransForm";
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        {/* <Route path="/calculater" element={<Calculater/>} /> */}
-        <Route path='/tranceform' element={<TransForm/>} />
-      </Routes>
+      <switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/calculater" element={<Calculater/>} /> */}
+          <Route path="/tranceform" element={<TransForm />} />
+        </Routes>
+      </switch>
     </Router>
   );
 }
